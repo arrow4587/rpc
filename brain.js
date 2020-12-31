f4aafa466ead14f706c4cda5814739045af91575
Karan <karankushwaha671@gmail.com>
2020-12-31 16:27:36 +0530

    added design to page

--- brain.js
+++ brain.js
@@ -1,66 +1,55 @@
 let weapons = ["rock", "paper", "scissor"];
 function computerPlay(weapons) {
-    let choice = Math.floor(Math.random() * (3 - 0)) + 0;
-    return choice;
+  let choice = Math.floor(Math.random() * (3 - 0)) + 0;
+  return choice;
 }
 
 function playRound(playerSelection, computerSelection) {
-    // your code here
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
-    if (playerSelection == computerSelection) {
-        console.log("TIE");
-        return 0;
+  // your code here
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+  if (playerSelection == computerSelection) {
+    console.log("TIE");
+    return 0;
+  } else {
+    if (playerSelection == "rock" && computerSelection == "scissor") {
+      console.log("win");
+      return 1;
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+      console.log("win");
+      return 1;
+    } else if (playerSelection == "scissor" && computerSelection == "paper") {
+      console.log("win");
+      return 1;
     } else {
-        if (playerSelection == "rock" && computerSelection == "scissor") {
-            console.log("win");
-            return 1;
-        } else if (playerSelection == "paper" && computerSelection == "rock") {
-            console.log("win");
-            return 1;
-        } else if (playerSelection == "scissor" && computerSelection == "paper") {
-            console.log("win");
-            return 1;
-        } else {
-            console.log("YOU LOOSE");
-            return -1;
-        }
+      console.log("YOU LOOSE");
+      return -1;
     }
+  }
 }
 function game() {
-    let pscore = 0;
-    let cscore = 0;
-    
-    let round=5;
-    for (let index = 0; index < 5; index++) {
-        let rscore=0;
-        const playerSelection = window.prompt("ENTER YOUR WEAPON")
-        x = computerPlay(weapons);
-        const computerSelection = weapons[x];
-        console.log(playerSelection, computerSelection)
-        rscore=playRound(playerSelection, computerSelection)
+  let pscore = 0;
+  let cscore = 0;
 
-        if(rscore==1)
-        {
-            pscore=pscore+1;
-        }
-        else if(rscore==0){
-            round=round-1
+  let round = 5;
+  for (let index = 0; index < 5; index++) {
+    let rscore = 0;
+    const playerSelection = window.prompt("ENTER YOUR WEAPON");
+    x = computerPlay(weapons);
+    const computerSelection = weapons[x];
+    console.log(playerSelection, computerSelection);
+    rscore = playRound(playerSelection, computerSelection);
 
-            
-        }
-        else
-        {
-            cscore=cscore+1
-        }
-
-
-
+    if (rscore == 1) {
+      pscore = pscore + 1;
+    } else if (rscore == 0) {
+      round = round - 1;
+    } else {
+      cscore = cscore + 1;
     }
-    console.log(pscore)
-    console.log(cscore)
-
-
+  }
+  console.log(pscore);
+  console.log(cscore);
 }
 
 game();
